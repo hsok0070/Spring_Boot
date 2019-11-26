2338f5f225a0ecfbb86d14f844f8cf3213cf2070
hsok0070 <rlaalsdn777@hanmir.com>
2019-11-26 14:16:07 +0900

    [Add]

---
+++ src/main/java/com/jo_test/controller.java
@@ -0,0 +1,16 @@
+package com.jo_test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+	
+	@GetMapping("/")
+	public String test() {
+		return "hello";
+		
+	}
+
+}
